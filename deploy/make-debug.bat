34fa28d9b920046cad1a362929260e643eb6454a
Kartonagnick <Kartonagnick@gmail.com>
2025-11-04 02:47:30 +0300

    add: deploy/make-debug.bat (001) PRE

---
+++ deploy/make-debug.bat
@@ -0,0 +1,17 @@
+@echo off & cls & echo. & echo.
+
+rem --- Kartonagnick/build_summary                       [deploy/make-debug.bat]
+rem [2025-11-04][03:40:00] 001 Kartonagnick PRE
+rem   --- local/mygtest                                  [deploy/make-debug.bat]
+rem   [2023-08-05][19:00:00] 001 Kartonagnick
+rem ============================================================================
+rem ============================================================================
+
+echo [START] please wait...
+::set "eTRACE=ON"
+::set "eDEBUG=ON"
+call "%~dp0make.bat" > "%~dp0log.txt" 2>&1
+echo [DONE]
+
+rem ============================================================================
+rem ============================================================================
