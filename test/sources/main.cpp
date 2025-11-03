f729379db6f081b77043f84e6c38e9bfa6e020a0
Kartonagnick <Kartonagnick@gmail.com>
2025-11-04 02:55:00 +0300

    add: test/sources/main.cpp (001) PRE

---
+++ test/sources/main.cpp
@@ -0,0 +1,20 @@
+
+// --- Kartonagnick/build_summary                                     [main.cpp]
+// [2025-11-04][03:40:00] 001 Kartonagnick PRE
+// [2025-11-04][00:51:20] registered on github
+// [2025-11-03][22:15:01] birthday of the project
+//==============================================================================
+//==============================================================================
+
+#include <mygtest/main.hpp>
+
+GTEST_API_ int main(int argc, char** argv)
+{
+    // example settings:
+    //   test.ext --gtest_filter=tools.stopwatch* --stress
+    //   testing::GTEST_FLAG(filter) = "tools.stopwatch_*";
+    return ::testing::run(argc, argv);
+}
+
+//==============================================================================
+//==============================================================================
